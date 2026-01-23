a3892a83d571c25095176e334706688f8756d544
lchiche <lchiche@student.42.fr>
2026-01-23 03:58:42 +0100

    update

--- includes/structs.hpp
+++ includes/structs.hpp
@@ -5,19 +5,19 @@
 #include <string>
 
 struct HeroClass {
-    std::string name;
-    int hp, speed, defense, shield, atk_damage, crit_rate, crit_damage, dodge_rate;
+	std::string name;
+	int hp, speed, defense, shield, atk_damage, crit_rate, crit_damage, dodge_rate;
 };
 
 enum class TextColor {
-    DEFAULT,
-    RED,
-    GREEN,
-    YELLOW,
-    BLUE,
-    MAGENTA,
-    CYAN,
-    WHITE
+	DEFAULT,
+	RED,
+	GREEN,
+	YELLOW,
+	BLUE,
+	MAGENTA,
+	CYAN,
+	WHITE
 };
 
 # endif
